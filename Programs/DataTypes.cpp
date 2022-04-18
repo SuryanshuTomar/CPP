f1322a4d492c38c7fcf15b66ef94884283e90669
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-18 11:09:54 +0530

    Data types code

---
+++ Programs/DataTypes.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+using namespace std;
+
+int main(){
+   // Data Types - char, int, float, double, bool
+   // Other Data Types - long int(or long), long long int(or long long)
+   char c = 'a';
+   int a = 3;
+   double b = 2.3;
+   a = 23.43;
+   bool d = true;
+   long e = 1323423;
+   cout << c << " " << a << " " << b << " " << d << " " << e << "\n\n";
+}
