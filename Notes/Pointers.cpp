8bd4b384552c9ded989c9b6e7464eda4a845cf60
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-03 21:18:38 +0530

    Basic Pointers

--- Notes/Pointers.cpp
+++ Notes/Pointers.cpp
@@ -3,4 +3,9 @@
 // - Pointers can only be declared the same data type it has been referenced to. Like - an integer pointer can only points to an integer value.
 // - We can also deference a pointer to get the value that the pointer memory address is holding and change its value also. And this is called dereferencing of a pointer.
 // - It is must for Pointers to assign a value which points to a similar data type memory address otherwise pointers can not be used for dereferencing.
-// -
+
+
+
+
+// =>  What are void pointers ?
+// - 
