516ddc2a668b465bce65198d8ccc3a31b6d09d15
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-03 21:24:34 +0530

    what is symbol table

--- Notes/Pointers.cpp
+++ Notes/Pointers.cpp
@@ -1,11 +1,12 @@
-// => What are pointers ?
+// => What is Symbol Table? 
+// - Symbol Table is an Important Data Structure created and maintained by the compiler in order to keep track of semantics of vairables -> i.e - it stores information about the scope and binding information about names, information about instances of various entities such as variables, function names, classes, objects etc
+
+// => What are Pointers ?
 // - Pointers are containers used to store values just like variable. The only difference is that pointers are used to store a memory location
 // - Pointers can only be declared the same data type it has been referenced to. Like - an integer pointer can only points to an integer value.
 // - We can also deference a pointer to get the value that the pointer memory address is holding and change its value also. And this is called dereferencing of a pointer.
 // - It is must for Pointers to assign a value which points to a similar data type memory address otherwise pointers can not be used for dereferencing.
 
 
-
-
-// =>  What are void pointers ?
+// =>  What are Void Pointers ?
 // - 
