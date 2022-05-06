b3290de9c90a2f5b04484602bd4428922f1d1b17
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-06 11:47:23 +0530

    Pair notes

--- Notes/STL/pairs.cpp
+++ Notes/STL/pairs.cpp
@@ -3,6 +3,16 @@
 // - More on -
 // https://www.geeksforgeeks.org/pair-in-cpp-stl/
 
+// - Pair is used to combine together two values that may be different in type. Pair provides a way to store two heterogeneous objects as a single unit. It is basically used if we want to store tuples. The pair container is a simple container defined in <utility> header consisting of two data elements or objects. 
+
+// - The first element is referenced as ‘first’ and the second element as ‘second’ and the order is fixed (first, second).
+// - Pair can be assigned, copied, and compared. The array of objects allocated in a map or hash_map is of type ‘pair’ by default in which all the ‘first’ elements are unique keys associated with their ‘second’ value objects.
+// - To access the elements, we use variable name followed by dot operator followed by the keyword first or second.
+
+// - Syntax: 
+// pair (data_type1, data_type2) Pair_name
+
+// - Example -
 #include <bits/stdc++.h>
 using namespace std;
 
