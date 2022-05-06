20f6619810bf2fcf9d5725dac1036271c0f35e2c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-06 22:18:47 +0530

    Arrays of vectors and Vector of Pairs notes

--- Notes/STL/vectors.cpp
+++ Notes/STL/vectors.cpp
@@ -84,3 +84,73 @@ int main(){
 
    return 0;
 }
+
+// ----------------------------------------------------------------------------------------------------
+// -> Nested Vectors -
+
+// - Nested Vector Pair - 
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNestedVector(vector<pair<int, int>> &vp){
+   cout << "Size of vector : " << vp.size() << endl;
+   
+   for(int i = 0; i< vp.size(); i++){
+      cout << "{ " << vp[i].first << ", " << vp[i].second << " }" << endl;
+   }
+}
+int main(){
+   // Nested Vector of Pairs
+   vector<pair<int, int>> vp = {{1, 2}, {3, 4}, {5, 6}};
+
+   // Displaying Vector with Nested Pair
+   // printNestedVector(vp);
+
+   // Taking Input in Nested Vector Pair
+   vector <pair<int, int>> vp1;
+   int input_size;
+   cout << "Enter Input size for nested vector pair : ";
+   cin >> input_size;
+   for(int i = 0; i < input_size; i++){
+      int f, s;
+      cin >> f >> s;
+      vp1.push_back({f, s});
+   }
+   printNestedVector(vp1);
+
+   return 0;
+}
+
+// - Nested Vector Array - 
+
+int main(){
+   // Declaring Array of Vectors
+   vector<int> v[10]; // Array of 10 vectors are created with initial size of zero.
+
+   int N;
+   cout << "Enter Size of Array of Vectors : ";
+   cin >> N;
+   vector<int> va[N];
+   for(int i = 0; i < N; i++){
+      int x;
+      cout << endl << "Enter Vector size : ";
+      cin >> x;
+      cout << endl << "Enter Vector elements : ";
+      for(int j = 0; j < x; j++){
+         int y;
+         cin >> y;
+         va[i].push_back(y);
+      }
+   }
+
+   // Printing Array of Vectors
+   cout << "Array of Vectors : " << endl;
+   for (int i = 0; i < N; i++){
+      for(int j = 0; j < va[i].size(); j++){
+         cout << va[i][j] << " ";
+      }
+         cout << endl;
+   }
+   
+   return 0;
+}
