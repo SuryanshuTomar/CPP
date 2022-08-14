9a7d45430bdf820800a02d4736ee062f32f08074
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-08-14 10:21:20 +0530

    OOPs basics notes

---
+++ Notes/OOPs/Concepts.cpp
@@ -0,0 +1,17 @@
+// => What is Object Oriented Programming (OOP) ?
+// - Object Oriented Programming is a paradigm that provides many concepts such as inheritance, data binding, polymorphism etc.
+// - The programming paradigm where everything is represented as an object is known as truly object-oriented programming language. Smalltalk is considered as the first truly object-oriented programming language.
+
+// -> OOPs (Object Oriented Programming System) -
+// - Object means a real word entity such as pen, chair, table etc. Object-Oriented Programming is a methodology or paradigm to design a program using classes and objects. It simplifies the software development and maintenance by providing some concepts:
+// 1. Object
+// 2. Class
+// 3. Inheritance
+// 4. Polymorphism
+// 5. Abstraction
+// 6. Encapsulation
+
+// -> Advantage of OOPs over Procedure-oriented programming language -
+// 1. OOPs makes development and maintenance easier where as in Procedure-oriented programming language it is not easy to manage if code grows as the project size grows.
+// 2. OOPs provide data hiding whereas in Procedure-oriented programming language a global data can be accessed from anywhere.
+// 3. OOPs provide ability to simulate real-world event much more effectively. We can provide the solution of real word problem if we are using the Object-Oriented Programming language.
