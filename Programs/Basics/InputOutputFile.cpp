3616c0316ba7413e21f27574ebd56469b8a78e6d
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-22 11:12:28 +0530

    Input and output using text files

---
+++ Programs/Basics/InputOutputFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  int testcase{};
+  cin >> testcase;
+
+  cout << "Something" << endl;
+  cout << testcase << endl;
+}
