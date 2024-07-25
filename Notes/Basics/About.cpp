d798c52b9e7931fbec5b37689f8bb4cd0443c510
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-07-25 17:35:20 +0530

    Notes: about cpp

---
+++ Notes/Basics/About.cpp
@@ -0,0 +1,22 @@
+// => About -
+// 1. C++ is a general-purpose programming language that supports both
+// procedural and object oriented programming paradigm.
+// 2. C++ can perform both low-level and high-level programming, and that's
+// why it is essentially considered a mid-level language.
+
+// ---------------------------------------------------------------
+// => Modern C++ and the C++ Standard -
+
+// 1. Early 1970s - C programming language created by Dennis Ritchie.
+// 2. 1979 - Bjarne Stroustrup created "c with classes".
+// 3. 1983 - Name changed to c++.
+// 4. 1989 - First Commerical release of c++.
+// 5. 1998 - c++98 Standard.
+// 6. 2003 - c++03 Standard.
+// 7. 2011 - c++11 Standard. (Lots of new features announced).
+// 8. 2014 - c++14 Standard.
+// 9. 2017 - c++17 Standard.
+// 10. 2020 - c++20 Standard.
+
+// Pre C++11 standard -> This is called Classical C++.
+// From C++11 standard -> This is called Modern C++.
